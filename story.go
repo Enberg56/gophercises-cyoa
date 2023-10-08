c06a338f285c3bc5e9d857bc1031182b1d6e5106
Enberg56 <emilnberg@gmail.com>
2023-10-08 15:28:06 +0200

    Added var HandelerTmpl

--- story.go
+++ story.go
@@ -5,6 +5,27 @@ import (
 	"io"
 )
 
+var defaultHandelerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Choose Your Own Adventure</title>
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+        <p>{{.}}</p>
+        {{end}}
+        <ul>
+        {{range .Options}}
+            <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+    </body>
+</html>
+`
+
 func JsonDecoder(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
 	var story Story
